f5a5069fa46ec0d9c0e32ffcfb535e79619ed710
Milan Dobrota <xcxmdobrota@tribune.com>
2011-01-15 12:06:36 -0600

    switching to YAML

--- lib/copyrb.rb
+++ lib/copyrb.rb
@@ -1,12 +1,13 @@
-require 'json'
+require 'yaml'
+require 'base64'
 
 def copy(obj)
-  text = obj.to_json
+  text = Base64.encode64(YAML.dump(obj)).gsub("\n", '') # getting rid of nonmeaningful new lines
   IO.popen('pbcopy', 'r+') { |clipboard| clipboard.puts text }
 end
 
 def paste
   text = IO.popen('pbpaste', 'r+') { |clipboard| clipboard.gets }
-  JSON.parse(text)
+  YAML.load(Base64.decode64(text))
 end
 
