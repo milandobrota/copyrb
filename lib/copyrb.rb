358bd99256c763d8317f6cb50f45e9138f843837
Milan Dobrota <xcxmdobrota@tribune.com>
2011-01-15 12:32:17 -0600

    it is very important that the comments are gramatically correct

--- lib/copyrb.rb
+++ lib/copyrb.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'base64'
 
 def copy(obj)
-  text = Base64.encode64(YAML.dump(obj)).gsub("\n", '') # getting rid of nonmeaningful new lines
+  text = Base64.encode64(YAML.dump(obj)).gsub("\n", '') # getting rid of meaningless new lines
   IO.popen('pbcopy', 'r+') { |clipboard| clipboard.puts text }
 end
 
