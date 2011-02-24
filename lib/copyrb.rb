9cab80e44cf18239c46a491dc474040f41045d55
Jan Lelis <mail@janlelis.de>
2011-02-24 17:57:52 +0100

    platform independent

--- lib/copyrb.rb
+++ lib/copyrb.rb
@@ -1,13 +1,14 @@
 require 'yaml'
 require 'base64'
+require 'clipboard'
 
 def copy(obj)
   text = Base64.encode64(YAML.dump(obj)).gsub("\n", '') # getting rid of meaningless new lines
-  IO.popen('pbcopy', 'r+') { |clipboard| clipboard.puts text }
+  Clipboard.copy(text)
 end
 
 def paste
-  text = IO.popen('pbpaste', 'r+') { |clipboard| clipboard.gets }
+  text = Clipboard.paste
   YAML.load(Base64.decode64(text))
 end
 
